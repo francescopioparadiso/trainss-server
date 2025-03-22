a50e329b4c21c1a74dc5df764e61605c9bac12c4
Francesco Pio Paradiso <71926962+francescopioparadiso@users.noreply.github.com>
2025-03-22 08:53:50 +0100

    Update server.py

--- server.py
+++ server.py
@@ -265,8 +265,7 @@ async def update_train_activity(update: TrainUpdate):
         if 'push_token' in content_state:
             del content_state['push_token']
         
-        logger.info(f"Questo è il primo debug: {fetch_parameter('stazioneUltimoRilevamento',content_state['numeroTreno'])}")
-        logger.info(f" Questo è il secondo debug: {content_state['numeroTreno']}")
+        content_state["stazioneUltimoRilevamento"] = fetch_parameter('stazioneUltimoRilevamento',content_state['numeroTreno'])
         
         # Create payload for APNs
         current_time = int(time.time())
